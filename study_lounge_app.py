719833b6cd6e67403def14b7342a503c2397d5b5
piarjayvic <piarjayvic@gmail.com>
2025-10-21 16:20:54 -0400

    Switch database driver to psycopg3

--- study_lounge_app.py
+++ study_lounge_app.py
@@ -16,6 +16,7 @@ if db_url.startswith("postgres://"):
     db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
 
 
+
 app.config["SQLALCHEMY_DATABASE_URI"] = db_url
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"connect_args": {"sslmode": "require"}}
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
